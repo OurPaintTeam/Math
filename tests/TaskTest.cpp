6d970bae72b1ed38e57aa656240ea8c8b344ba20
ardrass <st117289@student.spbu.ru>
2024-11-03 19:15:04 +0300

    added test for task

---
+++ tests/TaskTest.cpp
@@ -0,0 +1,89 @@
+//
+// Created by Eugene Bychkov on 25.10.2024.
+//
+#include <gtest/gtest.h>
+#include "Task.h"
+#include "Function.h"
+
+TEST(TaskTest, SingleVariableQuadratic) {
+    double x_value = 3.0;
+    Variable x(&x_value);
+    std::vector<Variable*> variables = {&x};
+
+    Constant* exponent = new Constant(2.0);
+    Power* function = new Power(&x, exponent);
+
+    Task task(function, variables);
+
+    double error = task.getError();
+    EXPECT_DOUBLE_EQ(error, x_value * x_value);
+
+    Matrix<> grad = task.gradient();
+    EXPECT_EQ(grad.row_size(), 1);
+    EXPECT_EQ(grad.col_size(), 1);
+    EXPECT_DOUBLE_EQ(grad(0, 0), 2 * x_value);
+
+    Matrix<> hessian = task.hessian();
+    EXPECT_EQ(hessian.row_size(), 1);
+    EXPECT_EQ(hessian.col_size(), 1);
+    EXPECT_DOUBLE_EQ(hessian(0, 0), 2.0);
+}
+TEST(TaskTest, TwoVariableQuadratic) {
+    double x_value = 3.0;
+    double y_value = 4.0;
+    Variable x(&x_value);
+    Variable y(&y_value);
+    std::vector<Variable*> variables = {&x, &y};
+
+    Power* x_squared = new Power(&x, new Constant(2.0));
+    Power* y_squared = new Power(&y, new Constant(2.0));
+    Addition* function = new Addition(x_squared, y_squared);
+
+    Task task(function, variables);
+
+    double error = task.getError();
+    EXPECT_DOUBLE_EQ(error, x_value * x_value + y_value * y_value);
+
+    Matrix<> grad = task.gradient();
+    EXPECT_EQ(grad.row_size(), 2);
+    EXPECT_EQ(grad.col_size(), 1);
+    EXPECT_DOUBLE_EQ(grad(0, 0), 2 * x_value);
+    EXPECT_DOUBLE_EQ(grad(1, 0), 2 * y_value);
+
+    Matrix<> hessian = task.hessian();
+    EXPECT_EQ(hessian.row_size(), 2);
+    EXPECT_EQ(hessian.col_size(), 2);
+    EXPECT_DOUBLE_EQ(hessian(0, 0), 2.0); // d^2f/dx^2
+    EXPECT_DOUBLE_EQ(hessian(0, 1), 0.0); // d^2f/dxdy
+    EXPECT_DOUBLE_EQ(hessian(1, 0), 0.0); // d^2f/dydx
+    EXPECT_DOUBLE_EQ(hessian(1, 1), 2.0); // d^2f/dy^2
+}
+
+TEST(TaskTest, TwoVariableMultiplication) {
+    double x_value = 2.0;
+    double y_value = 5.0;
+    Variable x(&x_value);
+    Variable y(&y_value);
+    std::vector<Variable*> variables = {&x, &y};
+
+    Multiplication* function = new Multiplication(&x, &y);
+
+    Task task(function, variables);
+
+    double error = task.getError();
+    EXPECT_DOUBLE_EQ(error, x_value * y_value);
+
+    Matrix<> grad = task.gradient();
+    EXPECT_EQ(grad.row_size(), 2);
+    EXPECT_EQ(grad.col_size(), 1);
+    EXPECT_DOUBLE_EQ(grad(0, 0), y_value); // df/dx = y
+    EXPECT_DOUBLE_EQ(grad(1, 0), x_value); // df/dy = x
+
+    Matrix<> hessian = task.hessian();
+    EXPECT_EQ(hessian.row_size(), 2);
+    EXPECT_EQ(hessian.col_size(), 2);
+    EXPECT_DOUBLE_EQ(hessian(0, 0), 0.0); // d^2f/dx^2
+    EXPECT_DOUBLE_EQ(hessian(0, 1), 1.0); // d^2f/dxdy
+    EXPECT_DOUBLE_EQ(hessian(1, 0), 1.0); // d^2f/dydx
+    EXPECT_DOUBLE_EQ(hessian(1, 1), 0.0); // d^2f/dy^2
+}
