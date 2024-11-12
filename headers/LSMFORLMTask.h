f3219a894213b67314ec1378f591e8f440e1acc3
ardrass <st117289@student.spbu.ru>
2024-11-12 22:30:08 +0300

    added LM using Eigen(just for test). It is working absolutely right and fast with non-linear functions and their system

---
+++ headers/LSMFORLMTask.h
@@ -0,0 +1,116 @@
+//
+// Created by Eugene Bychkov on 12.11.2024.
+//
+
+#ifndef MINIMIZEROPTIMIZER_LSMFORLMTASK_H
+#define MINIMIZEROPTIMIZER_LSMFORLMTASK_H
+#include "TaskF.h"
+#include <Eigen/Dense>
+#include <stdexcept>
+#include <utility>
+
+class LSMFORLMTask {
+    Function* c_function;
+    std::vector<Function*> m_functions;
+    std::vector<Variable*> m_X;
+
+public:
+    LSMFORLMTask(std::vector<Function*> functions, std::vector<Variable*> x)
+            : m_functions(std::move(functions)), m_X(std::move(x)) {
+        c_function = nullptr;
+        for (size_t i = 0; i < m_functions.size(); ++i) {
+            Constant* two = new Constant(2.0);
+            Power* squaredFunction = new Power(m_functions[i], two);
+            if (i == 0) {
+                c_function = squaredFunction;
+            } else {
+                c_function = new Addition(c_function, squaredFunction);
+            }
+        }
+    }
+
+    inline double getError() const {
+        return c_function->evaluate();
+    }
+
+    inline std::vector<double> getValues() const {
+        std::vector<double> values;
+        values.reserve(m_X.size());
+        for (const auto& x : m_X) {
+            values.push_back(x->evaluate());
+        }
+        return values;
+    }
+
+    double setError(const std::vector<double>& x) {
+        if (x.size() != m_X.size()) {
+            throw std::invalid_argument("Vector of variables has incorrect size.");
+        }
+        for (size_t i = 0; i < x.size(); ++i) {
+            m_X[i]->setValue(x[i]);
+        }
+        return c_function->evaluate();
+    }
+
+    Eigen::VectorXd gradient() const {
+        Eigen::VectorXd grad(m_X.size());
+        for (size_t i = 0; i < m_X.size(); ++i) {
+            Function* partialDerivative = c_function->derivative(m_X[i]);
+            grad(i) = partialDerivative->evaluate();
+            delete partialDerivative;
+        }
+        return grad;
+    }
+
+    Eigen::MatrixXd hessian() const {
+        Eigen::MatrixXd hessianMatrix(m_X.size(), m_X.size());
+        for (size_t i = 0; i < m_X.size(); ++i) {
+            for (size_t j = 0; j < m_X.size(); ++j) {
+                Function* secondPartialDerivative = c_function->derivative(m_X[i])->derivative(m_X[j]);
+                hessianMatrix(i, j) = secondPartialDerivative->evaluate();
+                delete secondPartialDerivative;
+            }
+        }
+        return hessianMatrix;
+    }
+
+    Eigen::MatrixXd jacobian() const {
+        Eigen::MatrixXd jac(m_functions.size(), m_X.size());
+        for (size_t i = 0; i < m_functions.size(); ++i) {
+            for (size_t j = 0; j < m_X.size(); ++j) {
+                Function* partialDerivative = m_functions[i]->derivative(m_X[j]);
+                jac(i, j) = partialDerivative->evaluate();
+                delete partialDerivative;
+            }
+        }
+        return jac;
+    }
+
+    std::pair<Eigen::VectorXd, Eigen::MatrixXd> linearizeFunction() const {
+        Eigen::VectorXd residuals(m_functions.size());
+        Eigen::MatrixXd jac(m_functions.size(), m_X.size());
+
+        for (size_t i = 0; i < m_functions.size(); ++i) {
+            residuals(i) = m_functions[i]->evaluate();
+            for (size_t j = 0; j < m_X.size(); ++j) {
+                Function* partialDerivative = m_functions[i]->derivative(m_X[j]);
+                jac(i, j) = partialDerivative->evaluate();
+                delete partialDerivative;
+            }
+        }
+        return {residuals, jac};
+    }
+
+    std::vector<Function*> getFunctions() const {
+        return m_functions;
+    }
+
+    ~LSMFORLMTask() {
+        delete c_function;
+        for (auto func : m_functions) {
+            delete func;
+        }
+    }
+};
+
+#endif //MINIMIZEROPTIMIZER_LSMFORLMTASK_H
---
+++ headers/Optimizers/LMForTest.h
@@ -0,0 +1,100 @@
+//
+// Created by Eugene Bychkov on 12.11.2024.
+//
+
+#ifndef MINIMIZEROPTIMIZER_LMFORTEST_H
+#define MINIMIZEROPTIMIZER_LMFORTEST_H
+
+#include "Optimizer.h"
+#include "LSMFORLMTask.h"
+#include <Eigen/Dense>
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+
+class LevenbergMarquardtSolver {
+private:
+    LSMFORLMTask* task;
+    Eigen::VectorXd result;
+    bool converged;
+    double currentError;
+    double lambda;
+    double b_increase;
+    double b_decrease;
+    double epsilon1;
+    double epsilon2;
+    int maxIterations;
+
+public:
+    LevenbergMarquardtSolver(double initLambda = 1.0, double b_increase = 2.0, double b_decrease = 2.0,
+                             double epsilon1 = 1e-6, double epsilon2 = 1e-6, int maxIterations = 100)
+            : lambda(initLambda), b_increase(b_increase), b_decrease(b_decrease), epsilon1(epsilon1), epsilon2(epsilon2),
+              maxIterations(maxIterations), converged(false) {}
+
+    void setTask(LSMFORLMTask* task) {
+        this->task = dynamic_cast<LSMFORLMTask*>(task);
+        if (!this->task) {
+            throw std::invalid_argument("Task is not of type LSMFORLMTask.");
+        }
+        result = Eigen::VectorXd::Zero(task->getValues().size());
+        currentError = task->getError();
+    }
+
+    void optimize() {
+        int iteration = 0;
+        Eigen::VectorXd gradient;
+        Eigen::MatrixXd hessian;
+        Eigen::MatrixXd jacobian;
+        Eigen::VectorXd residuals;
+
+        while (iteration < maxIterations) {
+            auto [residuals, jacobian] = task->linearizeFunction();
+
+            gradient = jacobian.transpose() * residuals;
+            double gradientNorm = gradient.norm();
+            if (gradientNorm < epsilon1) {
+                converged = true;
+                break;
+            }
+            hessian = jacobian.transpose() * jacobian;
+
+            Eigen::MatrixXd dampedHessian = hessian + lambda * Eigen::MatrixXd::Identity(hessian.rows(), hessian.cols());
+            Eigen::VectorXd delta = dampedHessian.ldlt().solve(gradient);
+            if (delta.norm() < epsilon1){
+                converged = true;
+                break;
+            }
+            Eigen::VectorXd newResult = result - delta;
+            std::vector<double> newParams(newResult.data(), newResult.data() + newResult.size());
+            double newError = task->setError(newParams);
+            if (newError < currentError) {
+                result = newResult;
+                currentError = newError;
+                lambda /= b_decrease;
+            } else {
+                lambda *= b_increase;
+            }
+            if (gradient.norm() < epsilon1 && (newResult - result).norm() < epsilon2) {
+                converged = true;
+                break;
+            }
+
+            ++iteration;
+        }
+        std::cout << "Levenberg-Marquardt converged after " << iteration << " iterations." << std::endl;
+    }
+
+    std::vector<double> getResult() const {
+        return std::vector<double>(result.data(), result.data() + result.size());
+    }
+
+    bool isConverged() const {
+        return converged;
+    }
+
+    double getCurrentError() const {
+        return currentError;
+    }
+};
+
+#endif //MINIMIZEROPTIMIZER_LMFORTEST_H
