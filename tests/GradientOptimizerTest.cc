483e5ace8c0a4a91ca0897855b85a2ae4c6a3d4d
Ibragim Oskanov <Oskanov-ibragim.06@mail.ru>
2025-02-10 19:07:06 +0300

    fix memleak in GradientOptimizerTest

--- tests/GradientOptimizerTest.cc
+++ tests/GradientOptimizerTest.cc
@@ -6,11 +6,11 @@
 TEST(OptimizerTest, SingleVariableQuadraticFunction) {
     //f(x) = (x - 3)^2
     double a = 0.0;
-    Variable *x = new Variable(&a);
+    Variable* x = new Variable(&a);
     Constant* c = new Constant(3);
     Constant* d = new Constant(2);
-    Subtraction e(x, c);
-    Function *f = new Power(&e, d);
+    Subtraction* e = new Subtraction(x->clone(), c);
+    Function* f = new Power(e, d);
     std::vector<Variable*> variables = { x };
     TaskF task(f, variables);
     GradientOptimizer optimizer(0.1, 1000); // learningRate=0.1, maxIterations=1000
@@ -33,17 +33,17 @@ TEST(OptimizerTest, MultiVariableQuadraticFunction) {
     // f(x, y) = (x - 2)^2 + (y + 5)^2
     double a = 1.0;
     double b = 2.0;
-    Variable x(&a);
-    Variable y(&b);
-    Constant c(2.0);
-    Constant d(5.0);
-    Subtraction e(&x, &c);
-    Addition g(&y, &d);
-    Power h(&e, &c);
-    Power i(&g, &c);
-    Addition f(&h, &i);
-    std::vector<Variable*> variables = { &x, &y };
-    TaskF task(&f, variables);
+    Variable* x = new Variable(&a);
+    Variable* y = new Variable(&b);
+    Constant* c = new Constant(2.0);
+    Constant* d = new Constant(5.0);
+    Subtraction* e = new Subtraction(x->clone(), c);
+    Addition* g = new Addition(y->clone(), d);
+    Power* h = new Power(e, c->clone());
+    Power* i = new Power(g, c->clone());
+    Addition* f = new Addition(h, i);
+    std::vector<Variable*> variables = { x, y };
+    TaskF task(f, variables);
 
     GradientOptimizer optimizer(0.1, 1000); // learningRate=0.1, maxIterations=1000
     optimizer.setTask(&task);
@@ -64,13 +64,13 @@ TEST(OptimizerTest, MultiVariableQuadraticFunction) {
 
 TEST(OptimizerTest, DoesNotConvergeWithHighLearningRate) {
     double a = 0.0;
-    Variable x(&a);
-    Constant b(1.0);
-    Constant c(2.0);
-    Subtraction d(&x, &b);
-    Power f(&d, &c); // e = (x - 1)^2
-    std::vector<Variable*> variables = { &x };
-    TaskF task(&f, variables);
+    Variable* x = new Variable(&a);
+    Constant* b = new Constant(1.0);
+    Constant* c = new Constant(2.0);
+    Subtraction* d = new Subtraction(x->clone(), b);
+    Power* f = new Power(d, c); // e = (x - 1)^2
+    std::vector<Variable*> variables = { x };
+    TaskF task(f, variables);
 
     GradientOptimizer optimizer(10.0, 100); // learningRate=10.0, maxIterations=100
     optimizer.setTask(&task);
@@ -104,13 +104,13 @@ TEST(OptimizerTest, OptimizeWithoutSettingTask) {
 TEST(OptimizerTest, AlreadyOptimal) {
     //f(x) = (x - 4)^2
     double a = 4.0;
-    Variable x(&a);
-    Constant b(4.0);
-    Constant c(2.0);
-    Subtraction g(&x, &b);
-    Power f(&g, &c);
-    std::vector<Variable*> variables = { &x };
-    TaskF task(&f, variables);
+    Variable* x = new Variable(&a);
+    Constant* b = new Constant(4.0);
+    Constant* c = new Constant(2.0);
+    Subtraction* g = new Subtraction(x->clone(), b);
+    Power* f = new Power(g, c);
+    std::vector<Variable*> variables = { x };
+    TaskF task(f, variables);
 
     GradientOptimizer optimizer(0.1, 1000);
     optimizer.setTask(&task);
@@ -126,9 +126,4 @@ TEST(OptimizerTest, AlreadyOptimal) {
     EXPECT_DOUBLE_EQ(result[0], 4.0);
 
     EXPECT_DOUBLE_EQ(finalError, 0.0);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
