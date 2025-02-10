c1cbe7e63b3026dafd69255fbfbb52679b81855a
Ibragim Oskanov <Oskanov-ibragim.06@mail.ru>
2025-02-10 19:17:02 +0300

    fix memleak in NewtinGaussSolverTest

--- tests/NewtonGaussSolverTests.cc
+++ tests/NewtonGaussSolverTests.cc
@@ -6,12 +6,12 @@
 TEST(OptimizerTest, SingleVariableQuadraticFunction) {
     //f(x) = (x - 3)^2
     double a = 0.0;
-    Variable x(&a);
-    Constant c(3);
-    Constant d(2);
-    Subtraction e(&x, &c);
-    Function *f = new Power(&e, &d);
-    std::vector<Variable*> variables = { &x };
+    Variable* x = new Variable(&a);
+    Constant* c = new Constant(3.);
+    Constant* d = new Constant(2.);
+    Subtraction* e = new Subtraction(x->clone(), c);
+    Function* f = new Power(e, d);
+    std::vector<Variable*> variables = { x };
     LSMTask task({f}, variables);
     NewtonGaussSolver optimizer(10000); // maxIterations=1000
     optimizer.setTask(&task);
@@ -33,16 +33,16 @@ TEST(OptimizerTest, MultiVariableQuadraticFunction) {
     // f(x, y) = (x - 2)^2 + (y + 5)^2
     double a = 1.0;
     double b = 2.0;
-    Variable x(&a);
-    Variable y(&b);
-    Constant c(2.0);
-    Constant d(5.0);
-    Subtraction e(&x, &c);
-    Addition g(&y, &d);
-    Power h(&e, &c);
-    Power i(&g, &c);
-    Function* f = new Addition(&h, &i);
-    std::vector<Variable*> variables = { &x, &y };
+    Variable* x = new Variable(&a);
+    Variable* y = new Variable(&b);
+    Constant* c = new Constant(2.0);
+    Constant* d = new Constant(5.0);
+    Subtraction* e = new Subtraction(x->clone(), c);
+    Addition* g = new Addition(y->clone(), d);
+    Power* h = new Power(e, c->clone());
+    Power* i = new Power(g, c->clone());
+    Function* f = new Addition(h, i);
+    std::vector<Variable*> variables = { x, y };
     LSMTask task( {f}, variables);
 
     NewtonGaussSolver optimizer(10000); // maxIterations=1000
@@ -64,12 +64,12 @@ TEST(OptimizerTest, MultiVariableQuadraticFunction) {
 
 TEST(OptimizerTest, DoesNotConvergeWithHighLearningRate) {
     double a = 0.0;
-    Variable x(&a);
-    Constant b(1.0);
-    Constant c(2.0);
-    Subtraction d(&x, &b);
-    Function* f = new Power(&d, &c); // (x - 1)^2
-    std::vector<Variable*> variables = { &x };
+    Variable* x = new Variable(&a);
+    Constant* b = new Constant(1.0);
+    Constant* c = new Constant(2.0);
+    Subtraction* d = new Subtraction(x->clone(), b);
+    Function* f = new Power(d, c); // (x - 1)^2
+    std::vector<Variable*> variables = { x };
     LSMTask task({f}, variables);
 
     NewtonGaussSolver optimizer(10000); // maxIterations=100
@@ -105,12 +105,12 @@ TEST(OptimizerTest, AlreadyOptimal) {
     //f(x) = x - 4
     //f(x) = y + 5
     double a = 4.0;
-    Variable x(&a);
-    Constant b(4.0);
-    Constant c(2.0);
-    Subtraction g(&x, &b);
-    Function* f = new Power(&g, &c);
-    std::vector<Variable*> variables = { &x };
+    Variable* x = new Variable(&a);
+    Constant* b = new Constant(4.0);
+    Constant* c = new Constant(2.0);
+    Subtraction* g = new Subtraction(x->clone(), b);
+    Function* f = new Power(g, c);
+    std::vector<Variable*> variables = { x };
     LSMTask task({f}, variables);
 
     NewtonGaussSolver optimizer(10000);
@@ -127,9 +127,4 @@ TEST(OptimizerTest, AlreadyOptimal) {
     EXPECT_DOUBLE_EQ(result[0], 4.0);
 
     EXPECT_DOUBLE_EQ(finalError, 0.0);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }*/
